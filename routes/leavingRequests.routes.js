166d4087644bb5471988effbcec4563c848e346a
abdelfatah2000 <abdelfattahmohamed596@gmail.com>
2022-12-14 18:35:10 +0200

    validation

--- routes/leavingRequests.routes.js
+++ routes/leavingRequests.routes.js
@@ -2,7 +2,12 @@ const app = require("express").Router();
 const isAuth = require("../config/isAuth");
 const requestsControllers = require("../controllers/leavingRequests.controllers");
 
-app.post("/applyLeaveRequest", isAuth(), requestsControllers.applyLeaveRequest);
+app.post(
+  "/applyLeaveRequest",
+  [body("reason").isString().isLength({ min: 5 }).trim()],
+  isAuth(),
+  requestsControllers.applyLeaveRequest
+);
 app.get("/getLeaveRequest", isAuth(), requestsControllers.getLeaveRequest);
 
 module.exports = app;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,43 @@
 const app = require("express").Router();
 const isAuth = require("../config/isAuth");
 const userController = require("../controllers/user.controllers");
+const { check, body } = require("express-validator");
 
 //registeration
-app.post("/register", userController.register);
+app.post(
+  "/register",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .normalizeEmail(),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+  ],
+  userController.register
+);
 //update user data
 app.put("/updateUser/:id", isAuth(), userController.updateUser);
 //login
-app.post("/login", userController.loginUser);
+app.post(
+  "/login",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email address.")
+      .normalizeEmail(),
+    body("password", "Password has to be valid.")
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+  ],
+  userController.loginUser
+);
 //checkin
-app.post("/userData", isAuth(), userController.getUser);
+app.get("/userData", isAuth(), userController.getUser);
 module.exports = app;
